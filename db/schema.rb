77ef7093e25b9dcad7f88c4d4e4b2f27341e82c1
MAximilianRaine <raine.maximilian@gmail.com>
2024-02-24 12:51:13 +0100

    added user colums migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_23_150155) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_24_114545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -87,6 +87,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_23_150155) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.text "description"
+    t.string "diet"
+    t.string "profile_picture_url"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

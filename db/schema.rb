faf43d5df2f482d594a8c52ed08f326bf55e5aea
Alexandra Cambiaso <acambiaso93@gmail.com>
2024-02-24 12:46:21 +0100

    dashboard model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_23_150155) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_24_114552) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_23_150155) do
     t.datetime "updated_at", null: false
     t.index ["recipe_id"], name: "index_cookbooks_on_recipe_id"
     t.index ["user_id"], name: "index_cookbooks_on_user_id"
+  end
+
+  create_table "dashboards", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_dashboards_on_user_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
@@ -93,6 +100,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_23_150155) do
 
   add_foreign_key "cookbooks", "recipes"
   add_foreign_key "cookbooks", "users"
+  add_foreign_key "dashboards", "users"
   add_foreign_key "recipe_ingredients", "ingredients"
   add_foreign_key "recipe_ingredients", "recipes"
   add_foreign_key "reviews", "recipes"

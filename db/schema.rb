3798b088efc88ffefde7b4a008df604f552be84b
MAximilianRaine <raine.maximilian@gmail.com>
2024-02-27 21:02:01 +0100

    migration for recipes & user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_27_183640) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_27_195133) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_27_183640) do
     t.string "video"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -106,6 +108,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_27_183640) do
   add_foreign_key "dashboards", "users"
   add_foreign_key "recipe_ingredients", "ingredients"
   add_foreign_key "recipe_ingredients", "recipes"
+  add_foreign_key "recipes", "users"
   add_foreign_key "reviews", "recipes"
   add_foreign_key "reviews", "users"
   add_foreign_key "user_ingredients", "ingredients"

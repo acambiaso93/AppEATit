c354cf76138fa2e43381ff123e387bef6302b65e
MAximilianRaine <raine.maximilian@gmail.com>
2024-03-16 17:24:07 +0100

    drop on recipe show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,6 +8,7 @@ class RecipesController < ApplicationController
     @user_ingredients = @user.user_ingredients.includes(:ingredient)
     @recipe_ingredient_ids = @recipe.ingredients.pluck(:id)
     @my_favorites = current_user.favorite_recipes
+    @profile = @user.profile
   end
 
   def create

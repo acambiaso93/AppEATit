4e49cdbb708d6964f21e0f62f95dcb450a11867e
Jakub Danilewicz <jakubdanilewicz@gmail.com>
2024-03-19 20:05:31 +0100

    -

--- app/javascript/controllers/typed_controller.js
+++ app/javascript/controllers/typed_controller.js
@@ -7,8 +7,8 @@ export default class extends Controller {
   connect() {
     console.log("type connected")
     new Typed(this.element, {
-      strings: ["Discover the best recipes<br> that your fridge has to offer."],
-      typeSpeed: 60,
+      strings: ['<span style="color:green;">tofu<br>chestnuts<br>eggplant<br>miso paste<br>cucumber<br>mushrooms<br>carrot<br>rice</span><br><br><br><br>Add any ingredient you have<br>to see matching recipes'],
+      typeSpeed: 80,
     })
   }
 }

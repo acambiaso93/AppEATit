0cc1339fcbd7b2bab889026af7d87645cdadecd2
Jakub Danilewicz <jakubdanilewicz@gmail.com>
2024-03-21 18:31:26 +0100

    m

--- app/javascript/controllers/typed_controller.js
+++ app/javascript/controllers/typed_controller.js
@@ -1,14 +1,16 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 import Typed from "typed.js";
 
-// Connects to data-controller="typed"
 export default class extends Controller {
-
-  connect() {
-    console.log("type connected")
-    new Typed(this.element, {
-      strings: ['<span style="color:green;">tofu<br>chestnuts<br>eggplant<br>miso paste<br>cucumber<br>mushrooms<br>carrot<br>rice</span><br><br><br><br>Add any ingredient you have<br>to see matching recipes'],
-      typeSpeed: 80,
-    })
+    connect() {
+      console.log("type connected");
+      setTimeout(() => {
+        new Typed(this.element, {
+          strings: [
+            '<span style="color:green;">tofu<br>chestnuts<br>eggplant<br>miso paste<br>cucumber<br>mushrooms<br>carrot<br>rice</span><br>',
+          ],
+          typeSpeed: [10] // Array of typeSpeed values for each part of the string
+        });
+      }, 2000); // 2000 milliseconds = 2 seconds
+    }
   }
-}

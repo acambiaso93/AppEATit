e20e7966005b37fe256e65256076eb05289f8ded
MAximilianRaine <raine.maximilian@gmail.com>
2024-03-22 18:08:02 +0100

    changes to kitchen cards and no ingredient controller

--- app/javascript/controllers/no_ingredient_controller.js
+++ app/javascript/controllers/no_ingredient_controller.js
@@ -1,14 +1,18 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 import Typed from "typed.js";
 
 export default class extends Controller {
+  static values = { count: Number }
 
   connect() {
-    console.log("no_ingredient connected")
-    new Typed(this.element, {
-      strings: ["Add three ingredients to see what your fridge has to offer."],
-      typeSpeed: 60,
-      
-    })
+    console.log("Current countValue:", this.countValue); // Should log 5
+
+    if (this.countValue < 3) {
+      console.log("Initializing Typed.js");
+      new Typed(this.element, {
+        strings: ["Add three ingredients to see what your fridge has to offer."],
+        typeSpeed: 60,
+      });
+    }
   }
 }
